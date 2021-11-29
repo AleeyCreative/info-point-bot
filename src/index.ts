855db68619b9457c3cdd81c16bbf4519af03eb27
Baba Ali AbdulAziz <frontdev4@stableshield.com>
2021-11-29 03:02:58 +0000

    feat: sets  bot after express server boots

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import setupBot from "./setupBot";
 
 const welcomeMarkup = `<h2> <center> Welcome to info-point-bot, telegram link is <a href="https://t.me/info_point_bot">t.me/info_point_bot</a></center></h2>`;
 
-setupBot();
 const app = express();
 app.get("/", (_, res) => res.send(welcomeMarkup));
 
-const port = process.env.PORT || 4000;
-app.listen(port, () => console.log("express server running successfully"));
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log("express server running successfully");
+  setupBot();
+});
